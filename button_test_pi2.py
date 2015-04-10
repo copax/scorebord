dce9f85669aa14832bcd339dc3f537cf770b7eed
Dan Kacenjar <dan.kacenjar@wolterskluwer.com>
2015-04-10 09:51:10 -0400

    Added constants, lockout and reset via master control button

--- button_test_pi2.py
+++ button_test_pi2.py
@@ -1,45 +1,68 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
+# Constants for Button pins
+PUSH_BUTTON_1 = 5
+PUSH_BUTTON_2 = 6
+PUSH_BUTTON_3 = 13
+PUSH_BUTTON_4 = 19
+
+MASTER_BUTTON = 17
+
+# Constants for LED pins
+LED_1 = 21
+LED_2 = 20
+LED_3 = 16
+LED_4 = 12
+
+inputs_locked = False
+led_activated = 0
+
+
 GPIO.setmode(GPIO.BCM)
 
 # Setup Inputs
-GPIO.setup(5, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-GPIO.setup(6, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-GPIO.setup(13, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-GPIO.setup(19, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(PUSH_BUTTON_1, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(PUSH_BUTTON_2, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(PUSH_BUTTON_3, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(PUSH_BUTTON_4, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
+GPIO.setup(MASTER_BUTTON, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
 
 # Setup Outputs 
-GPIO.setup(12, GPIO.OUT)
-GPIO.setup(16, GPIO.OUT)
-GPIO.setup(20, GPIO.OUT)
-GPIO.setup(21, GPIO.OUT)
+GPIO.setup(LED_1, GPIO.OUT)
+GPIO.setup(LED_2, GPIO.OUT)
+GPIO.setup(LED_3, GPIO.OUT)
+GPIO.setup(LED_4, GPIO.OUT)
 
 try:
-    while True:
-		if(GPIO.input(5) == 0):
-			print("PB 1 pressed")
-			GPIO.output(21, 1)
-			sleep(5)
-			GPIO.output(21, 0)
-
-		if(GPIO.input(6) == 0):
-			print("PB 2 pressed")
-			GPIO.output(20, 1)
-			sleep(5)
-			GPIO.output(20, 0)
-
-		if(GPIO.input(13) == 0):
-			print("PB 3 pressed")
-			GPIO.output(16, 1)
-			sleep(5)
-			GPIO.output(16, 0)
-
-		if(GPIO.input(19) == 0):
-			print("PB 4 pressed")
-			GPIO.output(12, 1)
-			sleep(5)
-			GPIO.output(12, 0)
-
+	while True:
+		if(inputs_locked == False):
+			if(GPIO.input(PUSH_BUTTON_1) == GPIO.LOW):
+				print("PB 1 pressed")
+				inputs_locked = True
+				GPIO.output(LED_1, GPIO.HIGH)
+				led_activated = LED_1
+			elif(GPIO.input(PUSH_BUTTON_2) == GPIO.LOW):
+				print("PB 2 pressed")
+				inputs_locked = True
+				GPIO.output(LED_2, GPIO.HIGH)
+				led_activated = LED_2
+			elif(GPIO.input(PUSH_BUTTON_3) == GPIO.LOW):
+				print("PB 3 pressed")
+				inputs_locked = True
+				GPIO.output(LED_3, GPIO.HIGH)
+				led_activated = LED_3
+			elif(GPIO.input(PUSH_BUTTON_4) == GPIO.LOW):
+				print("PB 4 pressed")
+				inputs_locked = True
+				GPIO.output(LED_4, GPIO.HIGH)
+				led_activated = LED_4
+		else:
+			if(GPIO.input(MASTER_BUTTON) == GPIO.LOW):
+				print("Master Button pressed")
+				GPIO.output(led_activated, GPIO.LOW)
+				inputs_locked = False
 except KeyboardInterrupt:
-    GPIO.cleanup()
+	GPIO.cleanup()
