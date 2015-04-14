ef077e6322d501af3c05ab2bb9a11db0a87c023c
Gerb Stralko <gerb.stralko@gmail.com>
2015-04-14 03:09:39 +0000

    serving static files

--- NodeJS-Express/app.js
+++ NodeJS-Express/app.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('app'));
+
 app.get('/', function (req, res) {
-	  res.send('Hello World!');
+	  res.sendFile('/app/index.html');
 });
 
 var server = app.listen(3000, function () {
 
 	  var host = server.address().address;
-	    var port = server.address().port;
+	  var port = server.address().port;
 
 	      console.log('Example app listening at http://%s:%s', host, port);
 
