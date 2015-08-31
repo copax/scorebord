3ccc81a8b9a5cd1e9b677e277c47ed6b1caaf6ff
Jamison Blose <jamison.blose@wolterskluwer.com>
2015-08-31 17:17:03 -0400

    Refactored for migration to the Pi.

--- db/install.sql
+++ db/install.sql
@@ -1,12 +1,14 @@
-DROP TABLE `rsak`.`sb_teams`;
-CREATE TABLE `rsak`.`sb_teams` (
+create schema if not exists scorebord;
+
+DROP TABLE `scorebord`.`sb_teams`;
+CREATE TABLE `scorebord`.`sb_teams` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `code` varchar(10) NOT NULL,
   `name` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`, `code`,`name`));
 
-drop table rsak.sb_game;
-create table rsak.sb_game (
+drop table scorebord.sb_game;
+create table scorebord.sb_game (
 	id int not null auto_increment,
 	game_id varchar(10) not null,
 	description varchar(10) not null,
@@ -14,16 +16,16 @@ create table rsak.sb_game (
 	unique key game_uk(game_id, description)
 );
 
-drop table rsak.sb_button;
-create table rsak.sb_button (
+drop table scorebord.sb_button;
+create table scorebord.sb_button (
 	id int not null auto_increment,
 	btn_id varchar(10) not null,
     primary key (id),
     unique key sb_button_uk (btn_id)
 );
 
-DROP TABLE rsak.sb_team_btn_game_x; 
-CREATE TABLE rsak.sb_team_btn_game_x (
+DROP TABLE scorebord.sb_team_btn_game_x; 
+CREATE TABLE scorebord.sb_team_btn_game_x (
 	id int not null auto_increment,
     team_code varchar(10) not null,
     btn_code varchar(10) not null,
@@ -32,8 +34,8 @@ CREATE TABLE rsak.sb_team_btn_game_x (
     unique key sb_team_btn_game_x_uk (team_code,btn_code,game_id)
 );
 
-DROP TABLE rsak.sb_category;
-CREATE TABLE rsak.sb_category (
+DROP TABLE scorebord.sb_category;
+CREATE TABLE scorebord.sb_category (
 	id int not null auto_increment,
     cat_id int not null,
     cat_name varchar(20) not null,    
@@ -41,14 +43,14 @@ CREATE TABLE rsak.sb_category (
     unique key sb_team_btn_game_x_uk (cat_id,cat_name)
 );
 
-drop table rsak.sb_current_team;
-create table rsak.sb_current_team(
+drop table scorebord.sb_current_team;
+create table scorebord.sb_current_team(
   code varchar(10) not null,
   primary key (code)
 );
 
-drop table rsak.sb_question;
-create table rsak.sb_question(
+drop table scorebord.sb_question;
+create table scorebord.sb_question(
   id int not null auto_increment,
   category_id int not null,
   answer varchar(50) not null,
