53a66bcf064fb8b8fe4586d0cf797c85a645193c
Lenny Krueger <Lenny.Krueger@wolterskluwer.com>
2015-08-31 16:19:05 -0400

    Updated script to use 2 functions to post to the 2 different urls and moved them to NOT post after every iteration of the main loop.

--- button_handler.py
+++ button_handler.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 from time import sleep
 import urllib2
+import json
 
 # Constants for Button pins
 PUSH_BUTTON_1 = 2
@@ -84,6 +85,16 @@ GPIO.output(LED_10, GPIO.LOW)
 GPIO.output(LED_11, GPIO.LOW)
 GPIO.output(LED_12, GPIO.LOW)
 
+def postToUrl(button_pressed):
+     url = 'http://rsak.proxzerk.com/api_scoreboard/index.php/setteam/' + button_pressed
+     response = urllib2.urlopen(url).read()
+     print json.dumps(response)
+
+def postReset():
+    url = 'http://rsak.proxzerk.com/api_scoreboard/index.php/resetteam/'
+    response = urllib2.urlopen(url).read()
+    print json.dumps(response)
+
 try:
     while True:
         if(inputs_locked == False):
@@ -92,97 +103,92 @@ try:
                 inputs_locked = True
                 GPIO.output(LED_1, GPIO.HIGH)
                 led_activated = LED_1
-                button_pressed = "BTN01"
+                postToUrl("BTN01")
                 continue
             elif(GPIO.input(PUSH_BUTTON_2) == GPIO.LOW):
                 print("PB 2 pressed")
                 inputs_locked = True
                 GPIO.output(LED_2, GPIO.HIGH)
                 led_activated = LED_2
-                button_pressed = "BTN02"
+                postToUrl("BTN02")
                 continue
             elif(GPIO.input(PUSH_BUTTON_3) == GPIO.LOW):
                 print("PB 3 pressed")
                 inputs_locked = True
                 GPIO.output(LED_3, GPIO.HIGH)
                 led_activated = LED_3
-                button_pressed = "BTN03"
+                postToUrl("BTN03")
                 continue
             elif(GPIO.input(PUSH_BUTTON_4) == GPIO.LOW):
                 print("PB 4 pressed")
                 inputs_locked = True
                 GPIO.output(LED_4, GPIO.HIGH)
                 led_activated = LED_4
-                button_pressed = "BTN04"
+                postToUrl("BTN04")
                 continue
             elif(GPIO.input(PUSH_BUTTON_5) == GPIO.LOW):
                 print("PB 5 pressed")
                 inputs_locked = True
                 GPIO.output(LED_5, GPIO.HIGH)
                 led_activated = LED_5
-                button_pressed = "BTN05"
+                postToUrl("BTN05")
                 continue
             elif(GPIO.input(PUSH_BUTTON_6) == GPIO.LOW):
                 print("PB 6 pressed")
                 inputs_locked = True
                 GPIO.output(LED_6, GPIO.HIGH)
                 led_activated = LED_6
-                button_pressed = "BTN06"
+                postToUrl("BTN06")
                 continue
             elif(GPIO.input(PUSH_BUTTON_7) == GPIO.LOW):
                 print("PB 7 pressed")
                 inputs_locked = True
                 GPIO.output(LED_7, GPIO.HIGH)
                 led_activated = LED_7
-                button_pressed = "BTN07"
+                postToUrl("BTN07")
                 continue
             elif(GPIO.input(PUSH_BUTTON_8) == GPIO.LOW):
                 print("PB 8 pressed")
                 inputs_locked = True
                 GPIO.output(LED_8, GPIO.HIGH)
                 led_activated = LED_8
-                button_pressed = "BTN08"
+                postToUrl("BTN08")
                 continue
             elif(GPIO.input(PUSH_BUTTON_9) == GPIO.LOW):
                 print("PB 9 pressed")
                 inputs_locked = True
                 GPIO.output(LED_9, GPIO.HIGH)
                 led_activated = LED_9
-                button_pressed = "BTN09"
+                postToUrl("BTN09")
                 continue
             elif(GPIO.input(PUSH_BUTTON_10) == GPIO.LOW):
                 print("PB 10 pressed")
                 inputs_locked = True
                 GPIO.output(LED_10, GPIO.HIGH)
                 led_activated = LED_10
-                button_pressed = "BTN10"
+                postToUrl("BTN10")
                 continue
             elif(GPIO.input(PUSH_BUTTON_11) == GPIO.LOW):
                 print("PB 11 pressed")
                 inputs_locked = True
                 GPIO.output(LED_11, GPIO.HIGH)
                 led_activated = LED_11
-                button_pressed = "BTN11"
+                postToUrl("BTN11")
                 continue
             elif(GPIO.input(PUSH_BUTTON_12) == GPIO.LOW):
                 print("PB 12 pressed")
                 inputs_locked = True
                 GPIO.output(LED_12, GPIO.HIGH)
                 led_activated = LED_12
-                button_pressed = "BTN12"
+                postToUrl("BTN12")
                 continue
-
-            #call the webservice and pass button_pressed (BTN01-12)
-            url = 'http://rsak.proxzerk.com/api_scoreboard/index.php/setteam/'+ button_pressed
-            response = urllib2.urlopen(url).read()
         else:
             if(GPIO.input(MASTER_BUTTON) == GPIO.LOW):
                 print("Master Button pressed")
                 inputs_locked = False
                 button_pressed = "resetteam"
-            #call the webservice and pass button_pressed (ressetteam)
-            url = 'http://rsak.proxzerk.com/api_scoreboard/index.php/resetteam/'
-            response = urllib2.urlopen(url).read()
+                postReset()
+
 except KeyboardInterrupt:
     GPIO.cleanup()
 
