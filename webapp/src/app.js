98f3e5b2971394a2537f1b1097bd01a40adcad7d
Shane Vorachek <shane.vorachek@wolterskluwer.com>
2015-09-01 16:17:17 -0400

    Questions are now displaying again.

--- webapp/src/app.js
+++ webapp/src/app.js
@@ -4,32 +4,7 @@
   controller('TriviaCtrl', ['$scope', '$http', 'QuestionService', 'SoundService', 'ngAudio', TriviaController]);
 
   function TriviaController($scope, $http, QuestionService, SoundService, ngAudio) {
-    $scope.categories = QuestionService.getQuestions().categories;
+    $scope.categories = QuestionService.getQuestions();
     $scope.audioFiles = SoundService.getAudioFiles();
-
-    $scope.deactivateAll 
-
-    //DEBUG:
-    //$scope.audio = ngAudio.load($scope.audioFiles[0]);
-    //$scope.audio.volume = 0.8;
-    //$scope.audio.play();
-
-
-
-    function deactivateAll() {
-      $scope.categories.forEach(function(category) {
-        category.questions.forEach(function(question) {
-          question.active = false;
-        });
-      });
-    }
-
-    document.addEventListener('keyup', (function(event) {
-      if (event.keyCode === 27) {
-        $scope.$apply(function() {
-          deactivateAll();
-        });
-      }
-    }));
   }
 })();
--- webapp/src/directives.js
+++ webapp/src/directives.js
@@ -1,37 +1,39 @@
 (function() {
   angular.
   module('app').
-  directive('triviaCategory', TriviaCategory).
-  directive('triviaQuestion', TriviaQuestion);
+  directive('triviaCategory', ['QuestionService', TriviaCategory]).
+  directive('triviaQuestion', ['QuestionService', TriviaQuestion]);
 
-  function TriviaCategory() {
+  function TriviaCategory(QuestionService) {
     return {
       restrict: 'E',
       templateUrl: '../templates/category.tpl.html',
       replace: true,
       scope: {
-
+        category: '=',
+        categories: '='
       },
       link: function($scope, elem, attr) {
-        
+
+      },
+      controller: function($scope) {
       }
     }
   }
 
-  function TriviaQuestion() {
+  function TriviaQuestion(QuestionService) {
     return {
       restrict: 'E',
       templateUrl: '../templates/question.tpl.html',
       replace: true,
       scope: {
-        question: '=',
-        deactivateAll: '&'
+        question: '='
       },
       link: function($scope, elem, attr, ctrl) {
         $scope.activate = activate;
 
         function activate(question) {
-          deactivateAll();
+          QuestionService.deactivateAll();
           question.active = true;
         }
       }
