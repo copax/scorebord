0161c2264f31e40755c66b1a777afd72e764fc33
Shane Vorachek <shane.vorachek@wolterskluwer.com>
2015-09-09 14:49:54 -0400

    Fixed the multi-polling issue.

--- webapp/src/round.ctrl.js
+++ webapp/src/round.ctrl.js
@@ -4,14 +4,14 @@
   controller('RoundCtrl', ['$scope', '$http', '$routeParams', RoundController]);
 
   function RoundController($scope, $http, $routeParams) {
-    console.log('new controller');
+    var pollInterval = setInterval(buttonPoll, 100);
 
     if ($routeParams && $routeParams.round) {
       $scope.nextRound = parseInt($routeParams.round) + 1;
     }
 
     function buttonPoll() {
-      $http.get('/api/index.php/fetchteam/12TM').then(function(response) {
+      $http.get('/api/index.php/fetchteam/2TMS').then(function(response) {
         if (response.data && response.data.team.length) {
           if (response.data.team[0].name !== $scope.team) {
             $scope.team = response.data.team[0];
@@ -24,11 +24,9 @@
       });
     }
 
-    var pollInterval = setInterval(buttonPoll, 100);
-
     $scope.$on('$destroy', function() {
-      clearInterval(buttonPoll);
-      pollInterval = 0;
+      clearInterval(pollInterval);
+      pollInterval = null;
       console.log(pollInterval);
     });
   }
