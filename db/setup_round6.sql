602c736da8dbf60697c9d4760a9f5c827de7e71a
Chris Copac <Chris.Copac@IN27LP517643.na.wkglobal.com>
2015-09-15 14:16:01 -0400

    SQL Scripts to change the team colors for the 2TMS rounds

---
+++ db/setup_round6.sql
@@ -0,0 +1,2 @@
+update sb_team_btn_game_x set team_code = 'LBLU' where game_id = '2TMS' and btn_code in ('BTN01', 'BTN02', 'BTN03', 'BTN04', 'BTN05', 'BTN06');
+update sb_team_btn_game_x set team_code = 'GRNN' where game_id = '2TMS' and btn_code in ('BTN07', 'BTN08', 'BTN09', 'BTN10', 'BTN11', 'BTN12');
