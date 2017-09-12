09919d8d0d4ac7b20f8bf8b693e7ce47a35cdf37
jblose <jamblose-github@yahoo.com>
2017-09-12 16:49:48 -0400

    Logic now pools

--- webapp_spy_v_spy/src/controllers/team.ctrl.js
+++ webapp_spy_v_spy/src/controllers/team.ctrl.js
@@ -1,9 +1,10 @@
 'use strict';
 
 angular.module('SpyVsSpy.TeamCtrl', ['ngRoute', 'ngCookies'])
-    .controller('TeamCtrl', ['$scope','$routeParams','$cookies',
-        function ($scope, $routeParams, $cookies) {
-    		
+    .controller('TeamCtrl', ['$scope','$routeParams','$cookies','$http','$route',
+        function ($scope, $routeParams, $cookies, $http, $route) {
+
+    		//Global Team Data
     		$scope.teamsData = [['1','Austin Powers','austin_powers.jpg'],
     			['2','KAOS','chaos.jpg'],
     			['3','Ethan Hunt','ethan_hunt.jpg'],
@@ -17,15 +18,23 @@ angular.module('SpyVsSpy.TeamCtrl', ['ngRoute', 'ngCookies'])
     			['11','Pink Panther','pink_panther.jpg'],
     			['12','Sherlock Holmes','sherlock_holmes.jpg']];
     		
+    		
+    		//Team Vars
     		$scope.teamCnts = [];
     		var teamMin = 1;
             var teamMax = $scope.teamsData.length;
-            console.log('max - ' + teamMax);
             var randomTeam = 0;
             $scope.teamLevel = 1;
 			$scope.TEAMNAME = "Top Secret";
 			$scope.IMG_FILE = "spykit.jpg";
-            
+			
+    		//Button Poll 
+    		var pollInterval = setInterval(buttonPoll, 1000);
+    		var pollUrl = '/api/index.php/fetchcurrentteam';
+    		
+    		var resetUrl = '/api/index.php/resetteam';
+    		
+            //Pull Cookies
             $scope.teamCnts = $cookies.getObject('cnts');
             $scope.teamLevel = $cookies.get('teamLvl');
 
@@ -38,35 +47,45 @@ angular.module('SpyVsSpy.TeamCtrl', ['ngRoute', 'ngCookies'])
                     $cookies.put('teamLvl',1);
                     console.log('Reset and Ready');
                 }
-                else {
-                	var openSpots = 0;
-                	for ( var i = 0; i < teamMax; i++){
-                		if ($scope.teamCnts[i] < $scope.teamLevel){
-                			openSpots++;
-                		}
-                	}
-                	console.log('openSpots - ' + openSpots);
-                	if (openSpots == 0){             		
-                		$scope.teamLevel++;
-                	}
-                	console.log('level - ' + $scope.teamLevel);
-
-                	var filled = 0;
-                	while (filled == 0){
-                		randomTeam = Math.floor( Math.random() * ((teamMax+1) - teamMin ) + teamMin);
-                		if ($scope.teamCnts[randomTeam -1] < $scope.teamLevel ){
-                			$scope.teamCnts[randomTeam -1]++;
-                			filled = 1;
-                			console.log('filled - ' + (randomTeam - 1));
-                			$scope.TEAMNAME = $scope.teamsData[randomTeam - 1][1];
-                			$scope.IMG_FILE = $scope.teamsData[randomTeam - 1][2];
+            }
+            
+            function buttonPoll() {
+                $http.get(pollUrl).then(function(response) {
+                  if (response.data && response.data.team.length) {
+                	  console.log('have team');
+                	  var openSpots = 0;
+                	  for ( var i = 0; i < teamMax; i++){
+                		  if ($scope.teamCnts[i] < $scope.teamLevel){
+                			  openSpots++;
+                		  }
+                	  }
+                	  console.log('openSpots - ' + openSpots);
+                	  if (openSpots == 0){             		
+                		  $scope.teamLevel++;
+                	  }
+                	  console.log('level - ' + $scope.teamLevel);
+                	  
+                	  var filled = 0;
+                	  while (filled == 0){
+                		  randomTeam = Math.floor( Math.random() * ((teamMax+1) - teamMin ) + teamMin);
+                		  if ($scope.teamCnts[randomTeam -1] < $scope.teamLevel ){
+                			  $scope.teamCnts[randomTeam -1]++;
+                			  filled = 1;
+                			  console.log('filled - ' + (randomTeam - 1));
+                			  $scope.TEAMNAME = $scope.teamsData[randomTeam - 1][1];
+                			  $scope.IMG_FILE = $scope.teamsData[randomTeam - 1][2];
                 		}
                 	}
                     
                 	$cookies.putObject('cnts',$scope.teamCnts);
                     $cookies.put('teamLvl',$scope.teamLevel);
+                	
+                    $http.post(resetUrl).then(function(response) {
+                    	console.log('reset');
 
-                }
+                    })
+                  }                
+                });
             }
-        }]);
+    }]);
                                                             
